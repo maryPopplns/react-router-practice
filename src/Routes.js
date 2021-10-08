8c0f6be036162397c4fea978e1cce1aa058564f3
Spencer Knight <spencer.knightusn@gmail.com>
2021-10-08 16:40:19 -0700

    testing

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,16 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import App from './App';
-import Profile from './Profile';
+import App from './App.js';
+import Profile from './Profile.js';
+import Test from './Test.js';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' component={App} />
-        <Route path='/profile' component={Profile} />
+        <Route path='/home' component={App}>
+          {/* <Route component={Profile} /> */}
+          <Route path='/test' component={Test} />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
